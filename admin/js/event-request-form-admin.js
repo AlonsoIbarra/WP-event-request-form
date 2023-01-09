f31eb1bb4bd05e2f5befdb57d0626cb60a86fd8c
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2023-01-08 23:16:37 -0600

    Create ajax call for row check in single view

--- admin/js/event-request-form-admin.js
+++ admin/js/event-request-form-admin.js
@@ -28,5 +28,38 @@
 	 * Although scripts in the WordPress core, Plugins and Themes may be
 	 * practising this, we should strive to set a better example in our own work.
 	 */
+	 $(document).ready(function() {
+		$("#erf_single_viev_table tr").click(function(event) {
+			let id = $(event.target).data('id');
+			let field = $(event.target).data('name');
+			var value = 0;
+			if ($(event.target).data('checked') == '0'){
+				value = 1
+			}
+			$.ajax({
+				type: "POST",
+				url: AdminEventRequestFormRequests.url,
+				data: {
+					key: AdminEventRequestFormRequests.key,
+					action: 'erf_check_field_row',
+					id: id,
+					field: field,
+					value: value,
+				},
+				success: function(response){
+					if(response.success){
+						$(event.target).attr('data-checked', value);
+						if(value){
+							$(event.target).removeClass("field-not-checked");
+							$(event.target).addClass("field-checked");	
+						}else{
+							$(event.target).addClass("field-not-checked");
+							$(event.target).removeClass("field-checked");	
+						}
+					}
+				}
+			});
+		});
+	});
 
 })( jQuery );
